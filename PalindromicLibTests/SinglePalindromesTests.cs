12f90e4d03d8e5fc02cd23cc9bed7d60d5b7ac4c
Dickson Souza <disouzam@yahoo.com.br>
2023-09-19 14:47:08 -0300

    Added several new test cases as InlineData instances, both for valid palindromes and for non-palindrome numbers

--- PalindromicLibTests/SinglePalindromesTests.cs
+++ PalindromicLibTests/SinglePalindromesTests.cs
@@ -23,7 +23,40 @@ public class SinglePalindromesTests
     }
 
     [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(4)]
+    [InlineData(5)]
+    [InlineData(6)]
+    [InlineData(7)]
+    [InlineData(8)]
+    [InlineData(9)]
+    [InlineData(11)]
+    [InlineData(22)]
+    [InlineData(33)]
+    [InlineData(44)]
+    [InlineData(55)]
+    [InlineData(66)]
+    [InlineData(77)]
+    [InlineData(88)]
+    [InlineData(99)]
+    [InlineData(101)]
+    [InlineData(111)]
     [InlineData(121)]
+    [InlineData(131)]
+    [InlineData(141)]
+    [InlineData(151)]
+    [InlineData(161)]
+    [InlineData(171)]
+    [InlineData(181)]
+    [InlineData(191)]
+    [InlineData(1001)]
+    [InlineData(1221)]
+    [InlineData(1771)]
+    [InlineData(2772)]
+    [InlineData(5775)]
+    [InlineData(57275)]
     public void CheckValidPalindromeNumbers(uint currentNumber)
     {
         // Act
@@ -34,7 +67,19 @@ public class SinglePalindromesTests
     }
 
     [Theory]
+    [InlineData(10)]
+    [InlineData(12)]
+    [InlineData(13)]
+    [InlineData(14)]
+    [InlineData(15)]
+    [InlineData(16)]
+    [InlineData(17)]
+    [InlineData(18)]
+    [InlineData(19)]
+    [InlineData(20)]
     [InlineData(123)]
+    [InlineData(56275)]
+
     public void CheckInvalidPalindromeNumbers(uint currentNumber)
     {
         // Act
