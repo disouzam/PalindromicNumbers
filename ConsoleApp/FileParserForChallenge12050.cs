9937a4d93c5fec6bf3a9c927fe533af246220009
Dickson Souza <disouzam@yahoo.com.br>
2023-09-20 13:46:07 -0300

    Implemented class FileParserForChallenge12050 and associated tests

--- ConsoleApp/FileParserForChallenge12050.cs
+++ ConsoleApp/FileParserForChallenge12050.cs
@@ -1,5 +1,41 @@
-﻿namespace ConsoleApp;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
-internal class FileParserForChallenge12050
+namespace ConsoleApp;
+
+public class FileParserForChallenge12050
 {
+    private List<int> listOfInputs = new List<int>();
+
+    public List<int> ListOfInputs
+    {
+        get
+        {
+            return listOfInputs;
+        }
+    }
+
+
+    public void ReadFile(string filePath)
+    {
+        using var fileContent = new StreamReader(filePath);
+
+        listOfInputs.Clear();
+
+        while (!fileContent.EndOfStream)
+        {
+            var lineContent = fileContent.ReadLine();
+            var input = Convert.ToInt32(lineContent);
+
+            if (input != 0)
+            {
+                listOfInputs.Add(input);
+            }
+            else
+            { 
+                break; 
+            }
+        }
+    }
 }
--- ConsoleAppTests/FileParserForChallenge12050Tests.cs
+++ ConsoleAppTests/FileParserForChallenge12050Tests.cs
@@ -1,12 +1,33 @@
-﻿using Xunit;
+﻿using System.IO;
+
+using ConsoleApp;
+
+using FluentAssertions;
+using FluentAssertions.Execution;
+
+using Xunit;
 
 namespace ConsoleAppTests;
 
 public class FileParserForChallenge12050Tests
 {
     [Fact]
-    public void Test1()
+    public void CheckReadFileBehaviorWithSampleInput()
     {
+        // Arrange
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "SampleInput.txt");
+        var fileParser = new FileParserForChallenge12050();
 
+        // Act
+        fileParser.ReadFile(filePath);
+
+        // Assert
+        using (new AssertionScope())
+        {
+            fileParser.ListOfInputs.Count.Should().Be(3);
+            fileParser.ListOfInputs[0].Should().Be(1);
+            fileParser.ListOfInputs[1].Should().Be(12);
+            fileParser.ListOfInputs[2].Should().Be(24);
+        }
     }
 }
