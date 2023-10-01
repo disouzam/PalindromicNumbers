69accd370aab154a92858ed7f0035fdff4435a35
Dickson Souza <disouzam@yahoo.com.br>
2023-10-01 18:21:01 -0300

    Introduced an internal static class to perform configuration setup

---
+++ ConsoleApp/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApp;
+
+internal static class Startup
+{
+    internal static void BuildConfig(IConfigurationBuilder builder)
+    {
+        builder.SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("consoleapp.settings.json", false, true)
+               .AddJsonFile($"consoleapp.settings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true)
+               .AddEnvironmentVariables();
+    }
+}
